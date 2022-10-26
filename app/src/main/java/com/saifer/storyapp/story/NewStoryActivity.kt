a0ba2481b011ca72f96368d12cd9fd08e1555862
sndff <sndff0110@gmail.com>
2022-10-27 03:27:13 +0700

    Initial, Designing UIs and Add Ids for every view inside layouts

---
+++ app/src/main/java/com/saifer/storyapp/story/NewStoryActivity.kt
@@ -0,0 +1,12 @@
+package com.saifer.storyapp.story
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saifer.storyapp.R
+
+class NewStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_story)
+    }
+}
